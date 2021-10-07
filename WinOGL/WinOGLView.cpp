7928ca24dc3ca1608e94e5eefb9d4575f0896e09
匡史 藤井 <e1c19079@oit.ac.jp>
2021-10-07 15:22:11 +0900

    第一回

--- WinOGL/WinOGLView.cpp
+++ WinOGL/WinOGLView.cpp
@@ -24,6 +24,10 @@ IMPLEMENT_DYNCREATE(CWinOGLView, CView)
 
 BEGIN_MESSAGE_MAP(CWinOGLView, CView)
 	ON_WM_LBUTTONDOWN()
+	ON_WM_CREATE()
+	ON_WM_DESTROY()
+	ON_WM_ERASEBKGND()
+	ON_WM_SIZE()
 END_MESSAGE_MAP()
 
 // CWinOGLView コンストラクション/デストラクション
@@ -48,7 +52,7 @@ BOOL CWinOGLView::PreCreateWindow(CREATESTRUCT& cs)
 
 // CWinOGLView 描画
 
-void CWinOGLView::OnDraw(CDC* /*pDC*/)
+void CWinOGLView::OnDraw(CDC* pDC)
 {
 	CWinOGLDoc* pDoc = GetDocument();
 	ASSERT_VALID(pDoc);
@@ -56,6 +60,27 @@ void CWinOGLView::OnDraw(CDC* /*pDC*/)
 		return;
 
 	// TODO: この場所にネイティブ データ用の描画コードを追加します。
+
+	wglMakeCurrent(pDC->m_hDC, m_hRC);
+
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT	/*	|	GL_DEPTH_BUFFER_BIT*/);
+
+	glColor3f(1.0, 1.0, 1.0);
+	glPointSize(5);
+	glVertex2f(clickX,clickY);
+	/*glBegin(GL_LINE_LOOP);
+
+	glVertex2f(-0.5, 0.5);
+	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, -0.5);
+	glVertex2f(-0.5, -0.5);*/
+	glEnd();
+
+	glFlush();
+	SwapBuffers(pDC->m_hDC);
+
+	wglMakeCurrent(pDC->m_hDC, NULL);
 }
 
 
@@ -86,6 +111,108 @@ CWinOGLDoc* CWinOGLView::GetDocument() const // デバッグ以外のバージ�
 void CWinOGLView::OnLButtonDown(UINT nFlags, CPoint point)
 {
 	// TODO: ここにメッセージ ハンドラー コードを追加するか、既定の処理を呼び出します。
+	CRect rect;
+	GetClientRect(rect);
+
+	clickX =(double)point.x/ (double)rect.Width();
+	clickY = (double)point.y/ (double)rect.Height();
+
+	clickY = (clickY - 1)*-1;
+	clickX = clickX * 2 - 1;
+	clickY = clickY * 2 - 1;
+
+	double hi;
+	if (rect.Width() > rect.Height()) {
+		hi = (double)rect.Width() / rect.Height();
+		clickX = clickX * hi;
+	}
+	else {
+		hi = (double)rect.Height() / rect.Width();
+		clickY = clickY * hi;
+	}
+
+	RedrawWindow();
 
 	CView::OnLButtonDown(nFlags, point);
+}
+
+
+int CWinOGLView::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CView::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	// TODO: ここに特定な作成コードを追加してください。
+	PIXELFORMATDESCRIPTOR pfd =
+	{
+		sizeof(PIXELFORMATDESCRIPTOR),
+		1,
+		PFD_DRAW_TO_WINDOW|
+		PFD_SUPPORT_OPENGL|
+		PFD_DOUBLEBUFFER,
+		PFD_TYPE_RGBA,
+		32,
+		0,0,0,0,0,0,
+		0,0,0,0,0,0,0,
+		24,
+		0,0,
+		PFD_MAIN_PLANE,
+		0,
+		0,0,0
+	};
+
+	CClientDC	clientDC(this);
+	int	pixelFormat = ChoosePixelFormat(clientDC.m_hDC, &pfd);
+	SetPixelFormat(clientDC.m_hDC, pixelFormat, &pfd);
+	m_hRC = wglCreateContext(clientDC.m_hDC);
+
+	return 0;
+}
+
+
+void CWinOGLView::OnDestroy()
+{
+	CView::OnDestroy();
+	wglDeleteContext(m_hRC);
+
+	// TODO: ここにメッセージ ハンドラー コードを追加します。
+}
+
+
+BOOL CWinOGLView::OnEraseBkgnd(CDC* pDC)
+{
+	// TODO: ここにメッセージ ハンドラー コードを追加するか、既定の処理を呼び出します。
+
+	return true;
+}
+
+
+void CWinOGLView::OnSize(UINT nType, int cx, int cy)
+{
+	CView::OnSize(nType, cx, cy);
+
+	// TODO: ここにメッセージ ハンドラー コードを追加します。
+	CClientDC clientDC(this);
+	wglMakeCurrent(clientDC.m_hDC, m_hRC);
+
+	glViewport(0, 0, cx, cy);
+	glMatrixMode(GL_PROJECTION);	//問5.2で考える内容
+	glLoadIdentity();
+
+	double hi;
+	if (cx<cy)
+	{
+		hi = (double)cy / cx;
+		glOrtho(-1, 1, -1*hi, 1 * hi, -100, 100);
+	}
+	else
+	{
+		hi = (double)cx / cy;
+		glOrtho(-1 * hi, 1 * hi, -1, 1, -100, 100);
+	}
+
+	glMatrixMode(GL_MODELVIEW);
+
+	RedrawWindow();
+	wglMakeCurrent(clientDC.m_hDC, NULL);
 }
